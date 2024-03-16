5b4f423f017f1e31400015de068c85ead677ad73
Thomas Grbic <tomgrbz@gmail.com>
2024-03-15 20:36:46 -0400

    Downgrade migration version to 1 on fresh install

--- app/src/main/java/com/cs4520/assignment4/data_layer/LocalDatabase.kt
+++ app/src/main/java/com/cs4520/assignment4/data_layer/LocalDatabase.kt
@@ -3,7 +3,7 @@ package com.cs4520.assignment4.data_layer
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ProductEntity::class], version = 2)
+@Database(entities = [ProductEntity::class], version = 1)
 abstract class LocalDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDAO
 }
